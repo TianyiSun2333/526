0d7b0620a3b548018a5183323c08ab936817215c
yufeilin <43329166+yufeilin@users.noreply.github.com>
2019-09-15 22:48:17 -0700

    CharacterController

---
+++ MovingMaster/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,232 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public GameObject mainWorldManager = null;
+
+    public GameObject operatableTarget = null;
+
+    WorldController mainWorldController = null;
+
+    // 1 向上
+    // 2 向右
+    // 3 向下
+    // 4 向左
+    int characterOrientation = 1;
+
+    // 主角当前所在坐标
+    Vector2Int currentCoordinate = new Vector2Int(0, 0);
+
+    // 主角的操作目标位置
+    Vector2Int targetCoordinate = new Vector2Int(0, 1);
+
+    public int[] fuck = new int[1];
+
+    // 正在执行动作标旗
+    bool isOperating = false;
+    // 正在推标旗
+    bool isPushing = false;
+    // 正在拉标旗
+    bool isPulling = false;
+    // 正在举标旗
+    bool isLifting = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainWorldController = mainWorldManager.GetComponent<WorldController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // 举起物体
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            if(isOperating)
+            {
+                // 如果正在进行动作
+                if(isLifting)
+                {
+                    // 正在举着物体
+                }
+                else
+                {
+                    // 如果正在推
+                }
+            }
+            else
+            {
+
+
+                isOperating = true;
+                isLifting = true;
+            }
+        }
+
+        // 准备推拉
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (characterOrientation != 1)
+            {
+                this.transform.rotation = Quaternion.Euler(0, 0, 0);
+                characterOrientation = 1;
+                targetCoordinate = currentCoordinate + new Vector2Int(0, 1);
+
+                if(mainWorldController.CheckTileOperatable(targetCoordinate))
+                {
+                    operatableTarget.SetActive(true);
+                    operatableTarget.transform.position = mainWorldController.GetTilePosition(targetCoordinate);
+                }
+                else
+                {
+                    operatableTarget.SetActive(false);
+                }
+            }
+
+            if (mainWorldController.CheckTilePassable(currentCoordinate + new Vector2Int(0,1)))
+            {               
+                this.transform.position = this.transform.position + new Vector3(0.0f, 0.0f, 2.5f);
+
+                currentCoordinate += new Vector2Int(0, 1);
+
+                targetCoordinate += new Vector2Int(0, 1);
+
+                if (mainWorldController.CheckTileOperatable(targetCoordinate))
+                {
+                    operatableTarget.SetActive(true);
+                    operatableTarget.transform.position = mainWorldController.GetTilePosition(targetCoordinate);
+                }
+                else
+                {
+                    operatableTarget.SetActive(false);
+                }
+            }            
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (characterOrientation != 3)
+            {
+                this.transform.rotation = Quaternion.Euler(0, 180, 0);
+                characterOrientation = 3;
+                targetCoordinate = currentCoordinate + new Vector2Int(0, -1);
+
+                if (mainWorldController.CheckTileOperatable(targetCoordinate))
+                {
+                    operatableTarget.SetActive(true);
+                    operatableTarget.transform.position = mainWorldController.GetTilePosition(targetCoordinate);
+                }
+                else
+                {
+                    operatableTarget.SetActive(false);
+                }
+            }
+
+            if (mainWorldController.CheckTilePassable(currentCoordinate + new Vector2Int(0, -1)))
+            {               
+                this.transform.position = this.transform.position - new Vector3(0.0f, 0.0f, 2.5f);
+
+                currentCoordinate += new Vector2Int(0, -1);
+
+                targetCoordinate += new Vector2Int(0, -1);
+
+                if (mainWorldController.CheckTileOperatable(targetCoordinate))
+                {
+                    operatableTarget.SetActive(true);
+                    operatableTarget.transform.position = mainWorldController.GetTilePosition(targetCoordinate);
+                }
+                else
+                {
+                    operatableTarget.SetActive(false);
+                }
+            }                
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (characterOrientation != 4)
+            {
+                this.transform.rotation = Quaternion.Euler(0, -90, 0);
+                characterOrientation = 4;
+                targetCoordinate = currentCoordinate + new Vector2Int(-1, 0);
+
+                if (mainWorldController.CheckTileOperatable(targetCoordinate))
+                {
+                    operatableTarget.SetActive(true);
+                    operatableTarget.transform.position = mainWorldController.GetTilePosition(targetCoordinate);
+                }
+                else
+                {
+                    operatableTarget.SetActive(false);
+                }
+            }
+
+            if (mainWorldController.CheckTilePassable(currentCoordinate + new Vector2Int(-1, 0)))
+            {               
+                this.transform.position = this.transform.position - new Vector3(2.5f, 0.0f, 0.0f);
+
+                currentCoordinate += new Vector2Int(-1, 0);
+
+                targetCoordinate += new Vector2Int(-1, 0);
+
+                if (mainWorldController.CheckTileOperatable(targetCoordinate))
+                {
+                    operatableTarget.SetActive(true);
+                    operatableTarget.transform.position = mainWorldController.GetTilePosition(targetCoordinate);
+                }
+                else
+                {
+                    operatableTarget.SetActive(false);
+                }
+            }              
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (characterOrientation != 2)
+            {
+                this.transform.rotation = Quaternion.Euler(0, 90, 0);
+                characterOrientation = 2;
+                targetCoordinate = currentCoordinate + new Vector2Int(1, 0);
+
+                if (mainWorldController.CheckTileOperatable(targetCoordinate))
+                {
+                    operatableTarget.SetActive(true);
+                    operatableTarget.transform.position = mainWorldController.GetTilePosition(targetCoordinate);
+                }
+                else
+                {
+                    operatableTarget.SetActive(false);
+                }
+            }
+
+            if (mainWorldController.CheckTilePassable(currentCoordinate + new Vector2Int(1, 0)))
+            {               
+                this.transform.position = this.transform.position + new Vector3(2.5f, 0.0f, 0.0f);
+
+                currentCoordinate += new Vector2Int(1, 0);
+
+                targetCoordinate += new Vector2Int(1, 0);
+
+                if (mainWorldController.CheckTileOperatable(targetCoordinate))
+                {
+                    operatableTarget.SetActive(true);
+                    operatableTarget.transform.position = mainWorldController.GetTilePosition(targetCoordinate);
+                }
+                else
+                {
+                    operatableTarget.SetActive(false);
+                }
+            }                
+        }
+    }
+}
---
+++ MovingMaster/Assets/Scripts/WorldController.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class WorldController : MonoBehaviour
+{
+    public int worldSize = 10;
+
+    public float tileSize = 2.5f;
+
+    TileController[,] tileArray = null;
+
+    GameObject tilePrefab = null;
+
+    Material[] tileMaterial = null;
+
+    Vector3 initialTilePosition = new Vector3();
+
+    public Vector3 GetTilePosition(Vector2Int paraCoordinate)
+    {
+        return initialTilePosition + new Vector3(paraCoordinate.x * tileSize, 0.0f, paraCoordinate.y * tileSize);
+    }
+
+    // 检查目标地砖是否为合法操作目标
+    public bool CheckTileOperatable(Vector2Int paraCoordinate)
+    {
+        bool res = true;
+
+        if (paraCoordinate.x < 0 || paraCoordinate.y < 0 || paraCoordinate.x >= worldSize || paraCoordinate.y >= worldSize)
+        {
+            res = false;
+        }
+
+        return res;
+    }
+
+    // 检查目标地点是否为可通行目标
+    public bool CheckTilePassable(Vector2Int paraCoordinate)
+    {
+        bool res = true;
+
+        if(paraCoordinate.x < 0 || paraCoordinate.y <0 || paraCoordinate.x >= worldSize || paraCoordinate.y >= worldSize)
+        {
+            res = false;
+        }
+
+        return res;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tileMaterial = new Material[2];
+        tileMaterial[0] = (Material)Resources.Load("Materials/WhiteTile");
+        tileMaterial[1] = (Material)Resources.Load("Materials/BlackTile");
+
+        tilePrefab = (GameObject)Resources.Load("Prefabs/Tile");
+
+        tileArray = new TileController[worldSize, worldSize];
+
+        
+
+        if(worldSize % 2==0)
+        {
+            initialTilePosition = new Vector3((float)-((worldSize / 2 - 0.5) * tileSize), 0.0f, (float)-((worldSize / 2 - 0.5) * tileSize));
+        }
+        else
+        {
+            initialTilePosition = new Vector3((float)-((worldSize / 2) * tileSize), 0.0f, (float)-((worldSize / 2) * tileSize));
+        }
+
+        for(int i=0;i< worldSize;i++)
+        {
+            for(int j=0;j< worldSize;j++)
+            {
+                Vector3 currentTilePosition = initialTilePosition + new Vector3(i * tileSize, 0.0f, j * tileSize);
+
+                GameObject currentTile = GameObject.Instantiate(tilePrefab);
+
+                currentTile.transform.SetParent(this.gameObject.transform);
+
+                currentTile.transform.localPosition = currentTilePosition;
+
+                currentTile.GetComponent<Renderer>().material = tileMaterial[(i + j) % 2];
+
+                tileArray[i, j] = currentTile.GetComponent<TileController>();
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
